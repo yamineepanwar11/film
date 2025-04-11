8f1f6ba8b10255ad4fd02a74128675fb9207747b
Yaminee Panwar <yamineepanwar998@gmail.com>
2025-04-11 16:03:12 +0530

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,21 @@
 import './Card.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import StaticExample from './card_dtl';
 function BasicExample2(Props) {
     console.log(Props, "<===prop")
     return (
-        <>
-            <div className='card'>
+    <>
+     <div className='card' onClick={StaticExample()}>
+                <div className='image1'>
+                    <img src={Props.data.Poster}></img>
+                </div>
                 <div className='txt'>
                     <p>{Props.data.Title}
                     </p>
-                    </div>
-                <div className='image1'>
-                    <img src={Props.data.Poster}></img>
-                    </div>
+                </div>
             </div>
-
-        </>
+    </>
+           
     );
 
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,20 @@ import NavScrollExample from "../components/Nav1";
 import './Home.css'
 import BasicExample1 from "../components/Dropdown";
 import BasicExample2 from "../components/Card";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function HOME() {
   let [data1, setData1] = useState([]);
-  async function fetchUser() {
-    let url = 'https://omdbapi.com/?apikey=4e9e8ed7&s=series';
-    const userResult = await axios.get(url);
-    console.log(userResult, "<=======result");
-    setData1(userResult.data.Search);
-  }
+  useEffect(() => {
+    async function fetchUser() {
+      let url = 'https://omdbapi.com/?apikey=4e9e8ed7&s=series';
+      const userResult = await axios.get(url);
+      console.log(userResult, "<=======result");
+      setData1(userResult.data.Search);
+    }
+    fetchUser()
+  }, []);
   console.log(data1, "<===d1")
   return (
     <div className="outer">
@@ -21,14 +24,12 @@ function HOME() {
         <h1>Movies Related In 2025</h1>
         <h4><b>Select Year</b></h4>
         <BasicExample1 />
-        <button onClick={fetchUser}> On click
-        </button>
-        <div className="card">
-        {
-          data1.map((movideData) => {
-            return <BasicExample2 data={movideData}/>
-          })
-        }
+        <div className="card_cntr">
+          {
+            data1.map((movideData) => {
+              return <BasicExample2 data={movideData} />
+            })
+          }
         </div>
       </div>
     </div>
